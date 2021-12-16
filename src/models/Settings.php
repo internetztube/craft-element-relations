5575c035e91ebc78860600c9a1ab81a22f382134
GregBowne <gbowne@quickbase.com>
2021-12-16 14:34:34 -0500

    now with console commands

---
+++ src/models/Settings.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace internetztube\elementRelations\models;
+
+
+use craft\base\Model;
+
+/**
+ * @package ElementRelations
+ */
+class Settings extends Model {
+
+    /**
+     * @var string Cache Duration used in a strtotime() function, like '5h' or '2 days' or '1 month'
+     */
+    public $cacheDuration = '2 days';
+
+    /**
+     * @var bool Whether to cache element relations in the db or load them every time
+     */
+    public $useCache = true;
+
+}
--- src/services/ElementRelationsService.php
+++ src/services/ElementRelationsService.php
@@ -2,6 +2,7 @@
 
 namespace internetztube\elementRelations\services;
 
+use Craft;
 use craft\base\Component;
 use craft\base\Element;
 use craft\base\ElementInterface;
@@ -10,35 +11,106 @@ use craft\db\Table;
 use craft\elements\Entry;
 use craft\helpers\ArrayHelper;
 use craft\helpers\Cp;
+use craft\models\EntryType;
+use craft\models\FieldLayout;
+use craft\models\Section;
 use craft\models\Site;
+use craft\services\Sections;
+use internetztube\elementRelations\ElementRelations;
+use internetztube\elementRelations\fields\ElementRelationsField;
 use internetztube\elementRelations\models\ElementRelationsModel;
 use internetztube\elementRelations\records\ElementRelationsRecord;
+use phpDocumentor\Reflection\Types\Void_;
+use Throwable;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveRecord;
+use yii\db\StaleObjectException;
 use yii\web\NotFoundHttpException;
 
 class ElementRelationsService extends Component
 {
 
-    public $cacheDuration = '2 days';
+    public    $cacheDuration = '2 days';
+    protected $settings;
+
+    public function __construct($config = [])
+    {
+        $this->settings = ElementRelations::$plugin->getSettings();
+        parent::__construct($config);
+    }
+
+    /**
+     * @throws Throwable
+     * @throws StaleObjectException
+     * @throws NotFoundHttpException
+     */
+    public static function refreshEntryRelations(Entry $entry): void
+    {
+        $settings = ElementRelations::$plugin->getSettings();
+        if ($settings->useCache) {
+            $relationsRecords = self::getRelatedRelationsRecords($entry->id, $entry->siteId);
+            $service = new ElementRelationsService();
+            foreach ($relationsRecords as $record) {
+                $element = Craft::$app->elements->getElementById($record->elementId, null, $record->siteId);
+                if (!$element) {
+                    self::deleteRelationsRecord($record);
+                }
+                $service->getRelations($element, $element->id, $element->siteId, 'default', true);
+            }
+        }
+    }
+
+    /**
+     * @param int $elementId
+     * @param int $siteId
+     * @return array|ActiveRecord[]
+     */
+    public static function getRelatedRelationsRecords(int $elementId, int $siteId): array
+    {
+        $settings = ElementRelations::$plugin->getSettings();
+        if ($settings->useCache) {
+            return ElementRelationsRecord::find()
+                ->where(['like', 'relations', $elementId])
+                ->andWhere(['siteId' => $siteId])
+                ->all();
+        }
+        return [];
+    }
+
+    /**
+     * @param ElementRelationsRecord $record
+     * @throws Throwable
+     * @throws StaleObjectException
+     */
+    public static function deleteRelationsRecord(ElementRelationsRecord $record): void
+    {
+        $settings = ElementRelations::$plugin->getSettings();
+        if ($settings->useCache) {
+            $record->delete();
+        }
+    }
 
     /**
      * @param Element|ElementInterface $element
      * @param int $elementId
      * @param int $siteId
      * @param string $size
-     * @param bool $force  Force the record to re-cache
-     * @return string
+     * @param bool $force Force the record to re-cache
+     * @return string|bool
      */
-    public function getRelations(Element $element, int $elementId, int $siteId, string $size = 'default', bool $force = false ): string
+    public function getRelations(Element $element, int $elementId, int $siteId, string $size = 'default', bool $force = false): string
     {
-        $staleCache = date('Y-m-d H:i:s',strtotime("$this->cacheDuration ago"));
+        $cacheDuration = $this->settings->cacheDuration ?? $this->cacheDuration;
+        $staleCache = date('Y-m-d H:i:s', strtotime("$cacheDuration ago"));
         $stale = false;
 
         $cachedRelations = self::getStoredRelations($elementId, $siteId);
         if (!empty($cachedRelations) && $cachedRelations->dateUpdated < $staleCache) {
             $stale = true;
         }
+        $request = Craft::$app->getRequest();
+        $isConsoleRequest = ($request->getIsConsoleRequest());
 
-        // @todo verify it's not out of date
         if ($force || !$cachedRelations || $stale) {
             $relations = self::getRelationsFromElement($element);
 
@@ -70,49 +142,43 @@ class ElementRelationsService extends Component
                 }
             }
 
-            if ($result->isEmpty()) { $result->push('<span style="color: #da5a47;">Unused</span>'); }
+            if ($result->isEmpty()) {
+                $result->push('<span style="color: #da5a47;">Unused</span>');
+            }
             $resultHtml = $result->implode('<br />');
 
             //set it or Update it
             $relationsIds = array_unique(ArrayHelper::getColumn($relations, 'id'));
             self::setStoredRelations($elementId, $siteId, $relationsIds, $resultHtml);
         } else {
-            $resultHtml = $cachedRelations->getResultHtml();
-        }
-        return $resultHtml;
-    }
-
-    /**
-     * @throws \Throwable
-     * @throws \yii\db\StaleObjectException
-     * @throws NotFoundHttpException
-     */
-    public static function refreshEntryRelations(Entry $entry) : void {
-        $relationsRecords = self::getRelatedRelationsRecords($entry->id, $entry->siteId);
-        if (!$element) throw new NotFoundHttpException;
-        $service = new ElementRelationsService();
-        foreach ($relationsRecords as $record) {
-            $element = \Craft::$app->elements->getElementById($record->elementId, null, $record->siteId);
-            if (!$element) {
-                self::deleteRelationsRecord($record);
+            if (!$isConsoleRequest) {
+                $resultHtml = $cachedRelations->getResultHtml();
             }
-            $service->getRelations($element, $elementId, $siteId, 'default', true);
+        }
+        if (!$isConsoleRequest) {
+            return $resultHtml;
+        } else {
+            return true;
         }
     }
 
     /**
-     * @param Element $sourceElement
+     * @param int $elementId
      * @param int $siteId
      * @return false|ElementRelationsModel
      */
-    public static function getStoredRelations(int $elementId, int $siteId) {
-        $elementRelationsRecord = self::getStoredRelationsRecord($elementId, $siteId);
+    public static function getStoredRelations(int $elementId, int $siteId)
+    {
+        $settings = ElementRelations::$plugin->getSettings();
+        if ($settings->useCache) {
+            $elementRelationsRecord = self::getStoredRelationsRecord($elementId, $siteId);
 
-        $elementRelationsModel = new ElementRelationsModel();
-        if ($elementRelationsRecord) {
-            $attributes = $elementRelationsRecord->getAttributes();
-            $elementRelationsModel->setAttributes($attributes, false);
-            return $elementRelationsModel;
+            $elementRelationsModel = new ElementRelationsModel();
+            if ($elementRelationsRecord) {
+                $attributes = $elementRelationsRecord->getAttributes();
+                $elementRelationsModel->setAttributes($attributes, false);
+                return $elementRelationsModel;
+            }
         }
         return false;
     }
@@ -120,53 +186,18 @@ class ElementRelationsService extends Component
     /**
      * @param int $elementId
      * @param int $siteId
-     * @return array|\yii\db\ActiveRecord|null
+     * @return array|ActiveRecord|null
      */
-    public static function getStoredRelationsRecord(int $elementId, int $siteId) {
-        return ElementRelationsRecord::find()
-            ->where(['elementId'=>$elementId])
-            ->andWhere(['siteId' => $siteId])
-            ->one();
-    }
-
-    /**
-     * @param int $elementId
-     * @param int $siteId
-     * @return array
-     */
-    public static function getRelatedRelationsRecords(int $elementId, int $siteId): array
+    public static function getStoredRelationsRecord(int $elementId, int $siteId)
     {
-        return ElementRelationsRecord::find()
-            ->where(['like','relations', $elementId])
-            ->andWhere(['siteId' => $siteId])
-            ->all();
-    }
-
-    /**
-     * @param ElementRelationsRecord $record
-     * @throws \Throwable
-     * @throws \yii\db\StaleObjectException
-     */
-    public static function deleteRelationsRecord(ElementRelationsRecord $record) {
-        $record->delete();
-    }
-
-    /**
-     * @param int $elementId
-     * @param int $siteId
-     * @param string $resultHtml
-     * @param ElementRelationsRecord $elementRelationsRecord
-     */
-    public static function setStoredRelations(int $elementId, int $siteId, array $relations, string $resultHtml)
-    {
-        if (!$elementRelationsRecord = self::getStoredRelationsRecord($elementId, $siteId)) {
-            $elementRelationsRecord = new ElementRelationsRecord();
+        $settings = ElementRelations::$plugin->getSettings();
+        if ($settings->useCache) {
+            return ElementRelationsRecord::find()
+                ->where(['elementId' => $elementId])
+                ->andWhere(['siteId' => $siteId])
+                ->one();
         }
-        $elementRelationsRecord->setAttribute('elementId', $elementId);
-        $elementRelationsRecord->setAttribute('siteId', $siteId);
-        $elementRelationsRecord->setAttribute('relations', implode(',', $relations));
-        $elementRelationsRecord->setAttribute('resultHtml', $resultHtml);
-        $elementRelationsRecord->save();
+        return null;
     }
 
     /**
@@ -184,24 +215,14 @@ class ElementRelationsService extends Component
 
         $site = $anySite ? '*' : $sourceElement->site;
 
-        return collect($elements)->map(function(int $elementId) use ($site) {
+        return collect($elements)->map(function (int $elementId) use ($site) {
             /** @var ?Element $relation */
             $relation = self::getElementById($elementId, $site);
-            if (!$relation) { return null; }
+            if (!$relation) {
+                return null;
+            }
             return self::getRootElement($relation, $site);
         })->filter()->values()->toArray();
-    }
-
-    /**
-     * @param Entry $entry
-     * @throws \Throwable
-     * @throws \yii\db\StaleObjectException
-     */
-    public static function clearRelatedCaches(Entry $entry) {
-        $relationsRecords = self::getRelatedRelationsRecords($entry->id, $entry->siteId);
-        foreach ($relationsRecords as $record) {
-            self::deleteRelationsRecord($record);
-        }
     }
 
     /**
@@ -209,14 +230,33 @@ class ElementRelationsService extends Component
      * @param $site
      * @return Element|null
      */
-    private static function getElementById (int $elementId, $site): ?Element
+    public static function getElementById(int $elementId, $site): ?Element
     {
         if (is_numeric($site)) {
-            $site = \Craft::$app->sites->getSiteById($site);
+            $site = Craft::$app->sites->getSiteById($site);
         }
         $result = (new Query())->select(['type'])->from(Table::ELEMENTS)->where(['id' => $elementId])->one();
-        if (!$result) { return null; } // relation is broken
+        if (!$result) {
+            return null;
+        } // relation is broken
         return $result['type']::find()->id($elementId)->anyStatus()->site($site)->one();
+    }
+
+    /**
+     * @param Element $element
+     * @param $site
+     * @return Element|null
+     */
+    private static function getRootElement(Element $element, $site): ?Element
+    {
+        if (!isset($element->ownerId) || !$element->ownerId) {
+            return $element;
+        }
+        $sourceElement = self::getElementById($element->ownerId, $site);
+        if (!$sourceElement) {
+            return null;
+        }
+        return self::getRootElement($sourceElement, $site);
     }
 
     public static function assetUsageInProfilePhotos(Element $sourceElement)
@@ -239,13 +279,17 @@ class ElementRelationsService extends Component
     public static function assetUsageInSEOmatic(Element $sourceElement)
     {
         $result = ['usedGlobally' => false, 'elements' => []];
-        $isInstalled = \Craft::$app->db->tableExists('{{%seomatic_metabundles}}');
-        if (!$isInstalled) { return false; }
+        $isInstalled = Craft::$app->db->tableExists('{{%seomatic_metabundles}}');
+        if (!$isInstalled) {
+            return false;
+        }
 
         $extractIdFromString = function ($input) {
-            if (!$input) { return; }
+            if (!$input) {
+                return;
+            }
             $result = sscanf($input, '{seomatic.helper.socialTransform(%d, ');
-            return (int) collect($result)->first();
+            return (int)collect($result)->first();
         };
 
         $globalQueryResult = (new Query)->select(['metaGlobalVars', 'metaSiteVars'])
@@ -253,16 +297,26 @@ class ElementRelationsService extends Component
             ->all();
 
         $result['usedGlobally'] = collect($globalQueryResult)
-            ->map(function($row) { return collect($row)->values(); })
+            ->map(function ($row) {
+                return collect($row)->values();
+            })
             ->flatten()
-            ->map(function($row) { return json_decode($row, true); })
+            ->map(function ($row) {
+                return json_decode($row, true);
+            })
             ->map(function ($row) use ($extractIdFromString) {
-                if (isset($row['seoImage'])) { return $extractIdFromString($row['seoImage']); }
-                if (isset($row['identity']['genericImageIds'])) { return $row['identity']['genericImageIds']; }
+                if (isset($row['seoImage'])) {
+                    return $extractIdFromString($row['seoImage']);
+                }
+                if (isset($row['identity']['genericImageIds'])) {
+                    return $row['identity']['genericImageIds'];
+                }
                 return null;
             })
             ->flatten()->filter()
-            ->map(function($row) { return (int) $row; })->unique()
+            ->map(function ($row) {
+                return (int)$row;
+            })->unique()
             ->contains($sourceElement->id);
 
         $fields = (new Query)->select(['handle', 'columnSuffix'])
@@ -278,34 +332,99 @@ class ElementRelationsService extends Component
 
         collect($fields)->each(function ($handle) use (&$foundElements, $extractIdFromString, $sourceElement) {
             $fieldHandle = sprintf('field_%s', $handle);
-            $rows = (new Query)->select(['elements.canonicalId', 'elements.id', 'siteId', 'title', 'content.'.$fieldHandle])
+            $rows = (new Query)->select(['elements.canonicalId', 'elements.id', 'siteId', 'title', 'content.' . $fieldHandle])
                 ->from(['content' => Table::CONTENT])
                 ->innerJoin(['elements' => Table::ELEMENTS], '[[elements.id]] = [[content.elementId]]')
-                ->where(['NOT', ['content.'.$fieldHandle => null]])
+                ->where(['NOT', ['content.' . $fieldHandle => null]])
                 ->all();
             collect($rows)->each(function ($row) use (&$foundElements, $extractIdFromString, $fieldHandle, $sourceElement) {
                 $data = json_decode($row[$fieldHandle]);
                 $id = $extractIdFromString($data->metaGlobalVars->seoImage);
-                if ($id !== $sourceElement->id) { return; }
+                if ($id !== $sourceElement->id) {
+                    return;
+                }
                 $foundElements->push(self::getElementById($row['canonicalId'] ?? $row['id'], $row['siteId']));
             });
         });
         $result['elements'] = collect($foundElements)
-            ->unique('canonialId')
+            ->unique('canonicalId')
             ->toArray();
         return $result;
     }
 
     /**
-     * @param Element $element
-     * @param $site
-     * @return Element|null
+     * @param int $elementId
+     * @param int $siteId
+     * @param array $relations
+     * @param string $resultHtml
      */
-    private static function getRootElement (Element $element, $site): ?Element
+    public static function setStoredRelations(int $elementId, int $siteId, array $relations, string $resultHtml)
     {
-        if (!isset($element->ownerId) || !$element->ownerId) { return $element; }
-        $sourceElement = self::getElementById($element->ownerId, $site);
-        if (!$sourceElement) { return null; }
-        return self::getRootElement($sourceElement, $site);
+        $settings = ElementRelations::$plugin->getSettings();
+        if ($settings->useCache) {
+            if (!$elementRelationsRecord = self::getStoredRelationsRecord($elementId, $siteId)) {
+                $elementRelationsRecord = new ElementRelationsRecord();
+            }
+            $elementRelationsRecord->setAttribute('elementId', $elementId);
+            $elementRelationsRecord->setAttribute('siteId', $siteId);
+            $elementRelationsRecord->setAttribute('relations', implode(',', $relations));
+            $elementRelationsRecord->setAttribute('resultHtml', $resultHtml);
+            $elementRelationsRecord->setAttribute('dateUpdated', date('Y-m-d H:i:s'));
+            $elementRelationsRecord->save();
+        }
+    }
+
+    /**
+     * @param string $siteId
+     * @return array|ActiveRecord[]
+     */
+    public function getAllRelations($siteId = '*'): array
+    {
+        return ElementRelationsRecord::find()
+//            ->where(['siteId' => $siteId])
+            ->all();
+    }
+
+    /**
+     * @param Entry $entry
+     * @throws Throwable
+     * @throws StaleObjectException
+     */
+    public static function clearRelatedCaches(Entry $entry)
+    {
+        $settings = ElementRelations::$plugin->getSettings();
+        if ($settings->useCache) {
+            $relationsRecords = self::getRelatedRelationsRecords($entry->id, $entry->siteId);
+            foreach ($relationsRecords as $record) {
+                self::deleteRelationsRecord($record);
+            }
+        }
+    }
+
+    /**
+     * @return array
+     * @throws InvalidConfigException
+     */
+    public function getRelatedElementEntryTypeIds(): array
+    {
+
+        $relatedEntryTypes = [];
+        $sections = Craft::$app->getSections();
+        $entryTypes = $sections->getAllEntryTypes();
+
+        foreach ($entryTypes as $entryType) {
+            $fieldLayout = $entryType->getFieldLayout();
+            // Loop through the fields in the layout to see if there is an ElementRelations field
+            if ($fieldLayout) {
+                $fields = $fieldLayout->getFields();
+                foreach ($fields as $field) {
+                    if ($field instanceof ElementRelationsField) {
+                        $relatedEntryTypes[$entryType->id] = $entryType->name;
+                    }
+                }
+            }
+        }
+        return $relatedEntryTypes;
+
     }
 }
