268d869c51aa7e4393d6341f15fc31bf35f17950
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2022-01-04 18:15:26 +0100

    Add m220103_164702_install.php.

---
+++ src/migrations/m220103_164702_install.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace internetztube\elementRelations\migrations;
+
+use Craft;
+use craft\db\Migration;
+use internetztube\elementRelations\records\ElementRelationsRecord;
+
+class m220103_164702_install extends Migration
+{
+    public function safeUp()
+    {
+        $table = ElementRelationsRecord::tableName();
+        if (!$this->db->tableExists($table)) {
+            $this->createTable($table, [
+                'id' => $this->primaryKey(),
+                'elementId' => $this->integer()->notNull(),
+                'relations' => $this->text(),
+                'dateCreated' => $this->dateTime()->notNull(),
+                'dateUpdated' => $this->dateTime()->notNull(),
+                'uid' => $this->uid(),
+            ]);
+
+            $this->createIndex(null, $table, ['elementId'], true);
+            $this->addForeignKey(null, $table, 'elementId', '{{%elements}}', 'id', 'CASCADE');
+
+            // Refresh the db schema caches
+            Craft::$app->db->schema->refresh();
+        }
+
+        return true;
+    }
+
+    public function safeDown()
+    {
+        $table = ElementRelationsRecord::tableName();
+        if ($this->db->tableExists($table)) {
+            $this->dropTable($table);
+            Craft::$app->db->schema->refresh();
+        }
+        return true;
+    }
+}
