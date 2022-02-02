5585aaee3176dc196df255ee67f0635f48958905
Greg Bowne <39066452+gbowne-quickbase@users.noreply.github.com>
2022-02-02 08:50:40 -0700

    Drop index to allow for siteId column to be dropped

--- src/migrations/m220104_164702_simplify_cache_strategy.php
+++ src/migrations/m220104_164702_simplify_cache_strategy.php
@@ -10,6 +10,12 @@ class m220104_164702_simplify_cache_strategy extends Migration
     public function safeUp()
     {
         $table = ElementRelationsRecord::tableName();
+        // drop previous index on 'siteId` which was built by the install so we can drop the column
+        foreach (Craft::$app->db->getSchema()->findIndexes($table) as $name => $index) {
+            if ($index['columns'] === ['siteId']) {
+                $this->dropIndex($name, $table);
+            }
+        }
         $this->dropColumn($table, 'siteId');
         $this->dropColumn($table, 'markup');
         $this->createIndex(null, $table, ['relations', 'dateUpdated']);
