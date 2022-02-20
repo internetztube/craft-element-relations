83e05851e25646f2134c3ad40c4720b57e046550
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2022-02-20 18:15:37 +0100

    Frontend: Only load one element relation at a time and add refresh button to element detail.

--- src/controllers/ElementRelationsController.php
+++ src/controllers/ElementRelationsController.php
@@ -4,6 +4,7 @@ namespace internetztube\elementRelations\controllers;
 
 use Craft;
 use craft\web\Controller;
+use internetztube\elementRelations\jobs\RefreshElementRelationsJob;
 use internetztube\elementRelations\services\CacheService;
 use internetztube\elementRelations\services\MarkupService;
 
@@ -19,5 +20,15 @@ class ElementRelationsController extends Controller
         $force = Craft::$app->request->getParam('force') === 'true';
         $elementRelations = CacheService::getElementRelationsCached($elementId, $force);
         return MarkupService::getMarkupFromElementRelations($elementRelations, $elementId, $siteId);
+    }
+
+    public function actionRefreshByElementId(): string
+    {
+        $elementId = (int) Craft::$app->request->getParam('elementId');
+        $job = new RefreshElementRelationsJob([
+            'elementIds' => [$elementId],
+            'force' => true,
+        ]);
+        return Craft::$app->queue->push($job);
     }
 }
--- src/fields/ElementRelationsField.php
+++ src/fields/ElementRelationsField.php
@@ -28,10 +28,10 @@ class ElementRelationsField extends Field implements PreviewableFieldInterface
 
     public function getTableAttributeHtml($value, ElementInterface $element): string
     {
-        return $this->_getLazyHtml($element);
+        return $this->_getLazyHtml($element, false);
     }
 
-    private function _getLazyHtml(ElementInterface $element): string
+    private function _getLazyHtml(ElementInterface $element, bool $elementDetail): string
     {
         $id = sprintf('%s-%s-%s', $element->id, $element->siteId, StringHelper::randomString(6));
         $endpoint = UrlHelper::actionUrl('element-relations/element-relations/get-by-element-id', [
@@ -39,14 +39,18 @@ class ElementRelationsField extends Field implements PreviewableFieldInterface
             'siteId' => $element->siteId,
             'size' => 'small',
         ], null, false);
+        $refreshEndpoint = UrlHelper::actionUrl('element-relations/element-relations/refresh-by-element-id', [
+            'elementId' => $element->id,
+        ], null, false);
+
         return Craft::$app->getView()->renderTemplate(
             'element-relations/_components/fields/Relations_lazy',
-            ['endpoint' => $endpoint, 'id' => $id]
+            ['endpoint' => $endpoint, 'id' => $id, 'refreshEndpoint' => $refreshEndpoint, 'elementDetail' => $elementDetail,]
         );
     }
 
     public function getInputHtml($value, ElementInterface $element = null): string
     {
-        return $this->_getLazyHtml($element);
+        return $this->_getLazyHtml($element, true);
     }
 }
