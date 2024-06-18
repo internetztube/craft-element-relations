b019f39bd077f3f9ae036f18288b64695baa63c0
frederic.koeberl <frederic.koeberl@dynatrace.com>
2024-06-18 17:16:31 +0200

    Fix migrations

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -4,43 +4,35 @@ namespace internetztube\elementRelations\migrations;
 
 use Craft;
 use craft\db\Migration;
-use craft\db\Table;
-use internetztube\elementRelations\records\ElementRelationsRecord;
+use internetztube\elementRelations\records\ElementRelationsCacheRecord;
 
 class Install extends Migration
 {
     public function safeUp()
     {
-        $table = ElementRelationsRecord::tableName();
-        if (!$this->db->tableExists($table)) {
-            $this->createTable($table, [
-                "id" => $this->primaryKey(),
-                "type" => $this->string(32),
-                "sourceElementId" => $this->integer()->null(),
-                "sourceSiteId" => $this->integer()->null(),
-                "targetElementId" => $this->integer()->notNull(),
-                "targetSiteId" => $this->integer()->null(),
-                "dateCreated" => $this->dateTime()->notNull(),
-                "dateUpdated" => $this->dateTime()->notNull(),
-                "uid" => $this->uid(),
-            ]);
+        $table = ElementRelationsCacheRecord::tableName();
+        $this->createTable($table, [
+            'id' => $this->primaryKey(),
+            'sourceElementId' => $this->integer()->notNull(),
+            'sourceSiteId' => $this->integer()->notNull(),
+            'sourcePrimaryOwnerId' => $this->integer()->notNull(),
+            'targetElementId' => $this->integer()->notNull(),
+            'targetSiteId' => $this->integer()->notNull(),
+            'customFieldUid' => $this->string(),
+            'fieldId' => $this->integer(),
+            'type' => $this->string()->notNull(),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid(),
+        ]);
 
-            $this->createIndex(null, $table, ["targetElementId", "targetSiteId", "type"], false);
-            $this->addForeignKey(null, $table, "sourceElementId", Table::ELEMENTS, "id", "CASCADE");
-            $this->addForeignKey(null, $table, "targetElementId", Table::ELEMENTS, "id", "CASCADE");
-            $this->addForeignKey(null, $table, "sourceSiteId", Table::SITES, "id", "CASCADE");
-            $this->addForeignKey(null, $table, "targetSiteId", Table::SITES, "id", "CASCADE");
-
-            // Refresh the db schema caches
-            Craft::$app->db->schema->refresh();
-        }
-
+        // No foreign keys, since elements can be linked that are not there anymore, like in Redactor, CkEditor, ....
         return true;
     }
 
     public function safeDown()
     {
-        $table = ElementRelationsRecord::tableName();
+        $table = ElementRelationsCacheRecord::tableName();
         if ($this->db->tableExists($table)) {
             $this->dropTable($table);
             Craft::$app->db->schema->refresh();
